feb807956206746479752312d5c0ba079ae79e93
Ubuntu <admin222@Nodeserver.xvwzomgq2jke3crpxzytualqcb.ix.internal.cloudapp.net>
2021-08-22 15:26:45 +0000

    [TEST]CICD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Im express NOw im dev')
+    res.send('Hello Im express NOw im dev TEST PROD')
 })
 
 const PORT = process.env.PORT || 5000;
