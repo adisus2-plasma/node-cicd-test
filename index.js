ab0074fb315d2959242b005d924e72114633061b
Ubuntu <admin22@admin22.mbruqgfn4r5e3ja4pdg2pz1ehf.ix.internal.cloudapp.net>
2021-08-24 13:20:43 +0000

    [UPDATE]prod api/get

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello Im express NOw im dev TEST PRODssssssssssssssssssssss')
 })
 
+app.get('/books', (req, res) => {
+  res.send('shit shit shit')
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is running in ${PORT}`));
